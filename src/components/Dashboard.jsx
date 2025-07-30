08b0a15cf0c030389b7d0148fe707204189404fd
Uday Singh Rana <udaysr2005@gmail.com>
2025-07-31 01:32:25 +0530

    check321

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import sortyapp from './assets/sortyapp.png';
-import light from './assets/light.png';
-import dark from './assets/dark.png';
-import "./App.css";
+import sortyapp from '../assets/sortyapp.png';
+import light from '../assets/light.png';
+import dark from '../assets/dark.png';
+import "../App.css";
 
 function Dashboard() {
   const navigate = useNavigate();
